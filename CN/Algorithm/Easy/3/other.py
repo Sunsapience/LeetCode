f88e92f9b5d7750eb9159def63baf793eba7683c
Wang LG <wlgzglb@outlook.com>
2018-10-31 15:45:06 +0800

    Update other.py

--- CN/Algorithm/Easy/3/other.py
+++ CN/Algorithm/Easy/3/other.py
@@ -1,1 +1,8 @@
-
+def isPalindrome(self, x):
+    if x<0 or (x%10==0 and x!=0):
+        return False
+    result = 0
+    while x>result:
+        result = result*10 + x%10
+        x = x//10
+    return result==x or result//10==x
